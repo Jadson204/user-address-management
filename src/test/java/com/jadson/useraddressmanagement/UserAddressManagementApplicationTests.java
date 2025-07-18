a737d3462f8d7547ab2efa2d6454ada627632521
jadson <jadsonhenrique@gmail.com>
2025-07-17 23:56:00 -0300

    feat: create initial spring setup

---
+++ src/test/java/com/jadson/useraddressmanagement/UserAddressManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jadson.useraddressmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAddressManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
